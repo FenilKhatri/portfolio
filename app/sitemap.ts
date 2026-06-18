2485ca1248b61a46daaf011387f5e347afe3dad2
Fenil Khatri <fenilkhatri931@gmail.com>
2026-06-18 18:26:44 +0530

    changed sitemap url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,13 +3,13 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://fenil-portfolio.vercel.app',
+      url: 'https://fenil-khatri.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://fenil-portfolio.vercel.app/projects',
+      url: 'https://fenil-khatri.vercel.app/projects',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
