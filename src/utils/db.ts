ed758fbe757b46d7119715a14105c64c4961dbe2
Fenil Khatri <fenilkhatri931@gmail.com>
2026-06-24 16:13:15 +0530

    Fix database connection handling and disable sync in production

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -11,9 +11,13 @@ export const connectDB = async () => {
     try {
         await sequelize.authenticate();
         console.log("Database connected successfully!");
-        await sequelize.sync({ alter: true });
-        console.log("Tables synced.")
+
+        if (process.env.NODE_ENV !== "production") {
+            await sequelize.sync({ alter: true });
+            console.log("Tables synced.");
+        }
     } catch (error) {
         console.error("Failed to connect database!", error);
+        throw error;
     }
 }
